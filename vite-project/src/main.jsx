4e10906d4b32bb431fa6bd5e5980440d6f3ddcaa
Mrunal6708 <mrunaljoshi075@gmail.com>
2026-02-20 22:57:38 +0530

    git add

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+//AIzaSyC-6SaUd8KVgCs70qeNW4H-R5C7A4dB0Pk

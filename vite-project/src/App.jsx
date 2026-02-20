5466f9e9085917c539a7ea361421236539fb6a95
Mrunal6708 <mrunaljoshi075@gmail.com>
2026-02-20 23:49:55 +0530

    git add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_GOOGLE_API_KEY)  
+//  console.log(import.meta.env.VITE_GOOGLE_API_KEY)  
+
+const App=()=> {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='codeGeneratorParentContainer'>
+      <div className='inputSectionContainer'>
+        <textarea className='textAreaInput' placeholder='Describe your react component...!'/>
+        <button className='generateButton'>Generate</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='PreviewSectionContainer'>
+        <div className='emptMessage'>
+          <p>Describe your component in the input field and click "Generate" to see the preview here.</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
